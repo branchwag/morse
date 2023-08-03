e683bb4cf20ec8e5ce81b51c6afd2f0da571d9b0
LeAnne B <branchleannelmb@gmail.com>
2023-08-03 14:53:01 -0400

    gets tailwind working by adding missing postcss config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Morse</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="bg-fuchsia-700">
+        <h1 className="text-3xl font-bold underline">Morse</h1>
+        <div className="card bg-fuchsia-700">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>Test para</p>
+        </div>
       </div>
     </>
   );
